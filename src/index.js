3678c0daef2a994826b11956e09c159319d4f191
Anton <anton.arboleda2gmail.com>
2016-10-04 18:13:10 -0700

    functionality to choose videos on page

--- src/index.js
+++ src/index.js
@@ -13,10 +13,16 @@ class App extends Component {
   constructor(props){
   	super(props);
 
-  	this.state = { videos: [] };
+  	this.state = { 
+  		videos: [],
+  		selectedVideo: null
+  	};
 
 	YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-		this.setState({ videos });
+		this.setState({ 
+			videos: videos,
+			selectedVideo: videos[0] 
+		});
 		//es6 shortcut for this.setState({ videos: videos })
 	});
   }
@@ -25,8 +31,10 @@ class App extends Component {
 	return ( 
 	  <div>
 		<SearchBar />
-		<VideoDetail video={this.state.videos[0]} />
-		<VideoList videos={this.state.videos} />
+		<VideoDetail video={this.state.selectedVideo} />
+		<VideoList 
+		  onVideoSelect={selectedVideo => this.setState({selectedVideo})  }	
+		  viwdeos={this.state.videos} />
 	  </div>
 	);
   }
